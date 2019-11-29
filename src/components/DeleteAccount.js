4caf0875a2843e9628c8d2cf3616d9df1bff9c18
gergohrubo <gergohrubo@gmail.com>
2019-11-29 14:02:31 +0100

    Logout done

--- src/components/DeleteAccount.js
+++ src/components/DeleteAccount.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {deleteAccount} from '../store/deleteAccount/actions'
+import { deleteAccount } from '../store/deleteAccount/actions'
 
 function mapStateToProps(reduxState) {
     return {
@@ -12,37 +12,21 @@ function mapStateToProps(reduxState) {
 
 
 class DeleteAccount extends Component {
-    clickHandler = event =>{
+    clickHandler = event => {
         event.preventDefault();
-        console.log("deleting")
-        console.log(this.props.login)
         this.props.dispatch(deleteAccount(this.props.login.profile.id, this.props.login.accessToken.jwt))
     }
 
-    componentDidMount(){
-        console.log('router props', this.props)
+    componentDidMount() {
 
     }
-    
+
     render() {
         return (
             <div>
-                <span> THERE IS NO WAY BACK </span>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+                <h1> Are you sure you want to delete your account? </h1>
+                <br />
+                <br />
                 <button onClick={this.clickHandler}> Delete your account (CAREFULL!!!)</button>
             </div>
         );

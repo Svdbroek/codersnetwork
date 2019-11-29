811d5d1d45589ca9f5e73e1a83c69889843c1742
gergohrubo <gergohrubo@gmail.com>
2019-11-29 13:48:01 +0100

    Removing conditional rendering at componentDidMount phase

--- src/components/DeveloperList.js
+++ src/components/DeveloperList.js
@@ -7,9 +7,7 @@ import Developer from "./Developer";
 // The "unconnected" inner component:
 class DevelopersList extends React.Component {
   componentDidMount() {
-    if (!this.props.devs) {
-      this.props.dispatch(fetchDevelopers);
-    }
+    this.props.dispatch(fetchDevelopers);
   }
 
   render() {
@@ -20,26 +18,21 @@ class DevelopersList extends React.Component {
         <h1>Codaisseur developers</h1>
         {
           loading
-          ? <p>Loading...</p>
-          : <div><p>We have {this.props.devs.count} developers!</p>
-      
-          {this.props.devs.rows.map((object)=>{return <Developer name={object.name} email ={object.email} id={object.id}/> })}
-          </div>
+            ? <p>Loading...</p>
+            : <div><p>We have {this.props.devs.count} developers!</p>
+
+              {this.props.devs.rows.map((object) => { return <Developer name={object.name} email={object.email} id={object.id} /> })}
+            </div>
         }
       </div>
     )
   }
 }
 
-// The wrapper component that connects to the Redux store
-//  and passes down props derived from the store's state
-//  to the inner component:
-
 function mapStateToProps(reduxState) {
-  // console.log("redux state?", reduxState);
   return {
     devs: reduxState.developers
   }
 }
-// ...which is what we export as the default (only) export
+
 export default connect(mapStateToProps)(DevelopersList);

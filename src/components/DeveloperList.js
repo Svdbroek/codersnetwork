c63d98bd3ca2804aeeefd6fb7785ecde86fa12bf
gergohrubo <gergohrubo@gmail.com>
2019-11-29 16:35:53 +0100

    Search function working

--- src/components/DeveloperList.js
+++ src/components/DeveloperList.js
@@ -6,22 +6,30 @@ import Developer from "./Developer";
 
 // The "unconnected" inner component:
 class DevelopersList extends React.Component {
+  state = {
+    searchWord: ''
+  }
   componentDidMount() {
     this.props.dispatch(fetchDevelopers);
   }
-
+  handleChange = (event) => {
+    console.log('event change', event.target.name)
+    this.setState({ [event.target.name]: event.target.value })
+  }
   render() {
     const loading = !this.props.devs;
-
     return (
       <div>
         <h1>Codaisseur developers</h1>
+        <p>Search for developer:</p> <input name="searchWord" value={this.state.searchWord} onChange={this.handleChange} />
         {
           loading
             ? <p>Loading...</p>
-            : <div><p>We have {this.props.devs.count} developers!</p>
+            : <div><p>We have {this.state.searchWord.length === 0 ? this.props.devs.count : this.props.devs.rows.filter((object) => object.name.indexOf(this.state.searchWord) !== -1).length} developers!</p>
+              {this.state.searchWord.length === 0 && this.props.devs.rows.map((object) => { return <Developer name={object.name} email={object.email} id={object.id} /> })}
 
-              {this.props.devs.rows.map((object) => { return <Developer name={object.name} email={object.email} id={object.id} /> })}
+              {this.state.searchWord.length !== 0 && this.props.devs.rows.filter((object) => object.name.indexOf(this.state.searchWord) !== -1)
+                .map(object => <Developer name={object.name} email={object.email} id={object.id} />)}
             </div>
         }
       </div>
@@ -36,3 +44,6 @@ function mapStateToProps(reduxState) {
 }
 
 export default connect(mapStateToProps)(DevelopersList);
+
+// {this.state.searchWord.length !== 0 && this.props.devs.rows.filter((object) => object.name.indexOf(this.state.searchWord) !== -1)
+//   .map(object => <Developer name={object.name} email={object.email} id={object.id} />)}
